171bae3c95da951711fc47f9b668d6a4672c085f
sheridanzzz <sheridangomes@ymail.com>
2020-06-10 08:29:18 +1000

    Setting up our React app

---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>TagStore</h1>
+        <p>A Modern Image Storage on the Cloud</p>
+      </div>
+    </div>
+  );
+}
