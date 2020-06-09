c947be6e6abc76c2fbe658378cf919349ab54779
sheridan gomes <sheridangomes@ymail.com>
2020-06-10 08:57:17 +1000

    setting up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { LinkContainer } from "react-router-bootstrap";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
