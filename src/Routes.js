2a52bd764ba0e973e872bef382905ec0cd3daa16
sheridanzzz <sheridangomes@ymail.com>
2020-06-10 10:21:06 +1000

    React image upload done

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Signup from "./containers/Signup";
+import ImageUpload from "./containers/ImageUpload";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
 import Home from "./containers/Home";
 
 export default function Routes() {
@@ -9,7 +12,15 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
-	  {/* Finally, catch all unmatched routes */}
+	  <Route exact path="/login">
+        <Login />
+      </Route>
+	  <Route exact path="/signup">
+        <Signup />
+      </Route>
+	  <Route exact path="/ImageUpload">
+        <ImageUpload />
+      </Route>
       <Route>
         <NotFound />
       </Route>
---
+++ src/containers/ImageUpload.js
@@ -0,0 +1,52 @@
+import React, { useRef, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
+import { onError } from "../libs/errorLib";
+import config from "../config";
+import "./ImageUpload.css";
+
+export default function ImageUpload() {
+  const file = useRef(null);
+  const history = useHistory();
+  const [content, setContent] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleFileChange(event) {
+    file.current = event.target.files[0];
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (file.current && file.current.size > config.MAX_ATTACHMENT_SIZE) {
+      alert(
+        `Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE /
+          1000000} MB.`
+      );
+      return;
+    }
+
+    setIsLoading(true);
+  }
+
+  return (
+    <div className="ImageUpload">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="file">
+          <ControlLabel>Image</ControlLabel>
+          <FormControl onChange={handleFileChange} type="file" />
+        </FormGroup>
+        <LoaderButton
+          block
+          type="submit"
+          bsSize="large"
+          bsStyle="primary"
+          isLoading={isLoading}
+        >
+          Upload
+        </LoaderButton>
+      </form>
+    </div>
+  );
+}
